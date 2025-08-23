34c1716b4639c9a440d3f94e5b4bdc2bd3999b9b
victoredet <edetvictor849@gmail.com>
2025-08-23 19:50:43 +0000

    (chore): start project

--- main.go
+++ main.go
@@ -1,22 +1,20 @@
-package main 
+package main
 
 import (
 	"fmt"
-"os/exec"
+	"os/exec"
 
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
-  "github.com/AlecAivazis/survey/v2"
 )
 
-
-
 func main() {
-	var rootCmd = &cobra.Command{
+	rootCmd := &cobra.Command{
 		Use:   "cloud-hut",
 		Short: "cloud Hut",
 	}
 
-	var apacheCmd = &cobra.Command{
+	apacheCmd := &cobra.Command{
 		Use:   "apache",
 		Short: "install and set up apache",
 		Run: func(cmd *cobra.Command, args []string) {
@@ -24,67 +22,64 @@ func main() {
 			prompt := &survey.Input{
 				Message: "What is your name?",
 			}
-			 survey.AskOne(prompt, &name)
+			survey.AskOne(prompt, &name)
 
 			fmt.Printf("Hello, %s!\n", name)
 		},
 	}
 
+	gitSetUp := &cobra.Command{
+		Use:   "git",
+		Short: "set up git",
+		Run: func(cmd *cobra.Command, args []string) {
+			var email string
+			var name string
+			emailH := &survey.Input{
+				Message: "Enter your git email address: ",
+			}
+
+			survey.AskOne(emailH, &email)
+
+			nameH := &survey.Input{
+				Message: "Enter your name: ",
+			}
+
+			survey.AskOne(nameH, &name)
+
+			// fmt.Println("Installing git...")
+			// gitInstaller := exec.Command("pkg", "install", "git")
+			// gitInstallerOutput, err := gitInstaller.Output()
+			//    if err != nil {
+			//      fmt.Println("Error:", err)
+			//      return
+			//    }
+			//    fmt.Println(string(gitInstallerOutput))
+
+			// set user.email
+			emailCmd := exec.Command("git", "config", "--global", "user.email", email)
+			if out, err := emailCmd.CombinedOutput(); err != nil {
+				fmt.Println("Error setting email:", err)
+				fmt.Println("Output:", string(out))
+				return
+			}
+
+			// Set user.name
+			nameCmd := exec.Command("git", "config", "--global", "user.name", name)
+			if out, err := nameCmd.CombinedOutput(); err != nil {
+				fmt.Println("Error setting name:", err)
+				fmt.Println("Output:", string(out))
+				return
+			}
 
 
-	var gitSetUp = &cobra.Command{
-    Use:   "git",
-    Short: "set up git",
-    Run: func(cmd *cobra.Command, args []string) {
-		var email string
-		var name string
-		emailH := &survey.Input{
-				Message:"Enter your git email address: ",
-		}
-
-    survey.AskOne(emailH, &email)
-
-    nameH := &survey.Input{
-				Message:"Enter your name: ",
-    }
-
-    survey.AskOne(nameH, &name)
-
-    //commandH := exec.Command("ls", "-la") // or any other command
-			
-		//install git 
-			gitInstaller := exec.Command("pkg", "install", "git")	
-			gitInstallerOutput, err := gitInstaller.Output()
-      if err != nil {
-        fmt.Println("Error:", err)
-        return
-      }
-      fmt.Println(string(gitInstallerOutput))
 
 
 
+		},
+	}
 
-
-
-
-
-			//
-		// commandH := exec.Command("git", "config", "--global", "user.email", email, "&&",   "git", "config", "--global", "user.name", name)
-		// out, err := commandH.Output()
-		// if err != nil {
-		// 	fmt.Println("Error:", err)
-		// 	return
-		// }
-		//
-		//  fmt.Println(string(out))
-		//
-  	//create ssh key and return it as cat 
-    },
-  }
-  
 	rootCmd.AddCommand(gitSetUp)
 	rootCmd.AddCommand(apacheCmd)
 
 	rootCmd.Execute()
 }
-
