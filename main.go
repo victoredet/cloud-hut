c075d57a12cde5d542d74e6b213b79300452e54a
victoredet <edetvictor849@gmail.com>
2025-08-27 02:50:46 +0000

    (chore): install node and composer

--- main.go
+++ main.go
@@ -189,11 +189,4 @@ func main() {
 	rootCmd.AddCommand(composerSetupCmd)
 	rootCmd.AddCommand(nodeSetupCmd)
 	rootCmd.Execute()
-
-	fmt.Println("Press Ctrl+C to exit...")
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c // Block here until Ctrl+C
-	fmt.Println("\nExiting.")
 }
