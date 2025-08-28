289576a9554103d35b0e64f50f4e1b14cd770341
victoredet <edetvictor849@gmail.com>
2025-08-28 11:00:09 +0000

    (chore): connect database

--- database/config.go
+++ database/config.go
@@ -1,2 +1,28 @@
 // Package database
 package database
+
+import (
+	"log"
+
+	"mobileHost/models"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	database, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error, could not connect to database")
+	}
+
+	// auto migrate project
+	err = database.AutoMigrate((&models.Project{}))
+	if err != nil {
+		log.Fatal("Error, could not migrate database")
+	}
+
+	DB = database
+}
