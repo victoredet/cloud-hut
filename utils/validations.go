2f4049a4049014352f710434912f3d3da704f70d
victoredet <edetvictor849@gmail.com>
2025-08-28 13:51:05 +0000

    (chore): create project

--- utils/validations.go
+++ utils/validations.go
@@ -5,7 +5,6 @@ import "fmt"
 
 func ValidateAddProjectType(projectType string) bool {
 	fmt.Println("project type", projectType)
-	// add flask and fastapi
 	return projectType == "laravel" ||
 		projectType == "php" ||
 		projectType == "node" ||
