9e5b660107a347efc814339dc8e79eafd9ab72ce
victoredet <edetvictor849@gmail.com>
2025-09-01 23:38:46 +0000

    (chore): create laravel project

--- projects/core.go
+++ projects/core.go
@@ -3,6 +3,8 @@ package projects
 
 import (
 	"fmt"
+	"os/exec"
+	"path/filepath"
 
 	"mobileHost/database"
 	"mobileHost/models"
@@ -38,4 +40,28 @@ func ListProjects() {
 	for _, project := range projects {
 		fmt.Printf("- %s (%s)\n", project.Name, project.Type)
 	}
+}
+
+func createLaravelProject(projectName string, gitRepo string) {
+	fmt.Println("Creating Laravel project:", projectName)
+
+	keyPath := filepath.Join("/", "var", "www")
+
+	projectDir := exec.Command("mkdir", "-p", keyPath, projectName)
+	err := projectDir.Run()
+	if err != nil {
+		fmt.Println("Error creating project directory:", err)
+		return
+	}
+
+	gitCloneCmd := exec.Command("git", "clone", gitRepo, filepath.Join(keyPath, projectName))
+	err = gitCloneCmd.Run()
+	if err != nil {
+		fmt.Println("Error cloning repository. Please make sure you have all the right permissions.")
+		fmt.Println("Error:", err)
+		return
+	}
+}
+
+func exposeProjectToInternet(projectName string) {
 }
--- utils/validations.go
+++ utils/validations.go
@@ -7,6 +7,10 @@ func ValidateAddProjectType(projectType string) bool {
 	fmt.Println("project type", projectType)
 	return projectType == "laravel" ||
 		projectType == "php" ||
+		projectType == "react" ||
+		projectType == "vue" ||
+		projectType == "next" ||
+		projectType == "nuxt" ||
 		projectType == "node" ||
 		projectType == "flask" ||
 		projectType == "fastapi"
